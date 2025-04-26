a00ad721205339ca266b49ff13e3f67d5be0b2ad
agarasia <agarasia@asu.edu>
2025-04-25 17:18:15 -0700

    FIX: Allowed CORS Preflight Response to allow Requests from Frontend

--- middleware/clone_repo/main.py
+++ middleware/clone_repo/main.py
@@ -1,7 +1,7 @@
 import os
 from clone_repo import clone_repo
 import config
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
@@ -21,29 +21,47 @@ def fetch_metrics(name, port, payload):
         return name, response.json()
     except Exception as e:
         return name, {"error": str(e)}
+    
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
 
-@app.route("/metrics", methods=["POST"])
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+@app.route("/metrics", methods=["OPTIONS", "POST"])
 def get_metrics():
     try:
-        data = request.get_json()
-        repo_url = data["repo_url"]
-        head_sha, repo_dir = clone_repo(repo_url)
+        if request.method == "OPTIONS":
+            return _build_cors_preflight_response()
+        elif request.method == "POST":
+            data = request.get_json()
+            repo_url = data["repo_url"]
+            head_sha, repo_dir = clone_repo(repo_url)
 
-        services = load_service_config()
-        payload = {config.payload_key: repo_url}
-        
-        
+            services = load_service_config()
+            payload = {config.payload_key: repo_url}
+            
+            
 
-        results = {}
-        with ThreadPoolExecutor(max_workers=len(services)) as executor:
-            futures = [executor.submit(fetch_metrics, name, port, payload) for port, name in services.items()]
-            for future in as_completed(futures):
-                name, result = future.result()
-                results[name] = result
+            results = {}
+            with ThreadPoolExecutor(max_workers=len(services)) as executor:
+                futures = [executor.submit(fetch_metrics, name, port, payload) for port, name in services.items()]
+                for future in as_completed(futures):
+                    name, result = future.result()
+                    results[name] = result
 
-        return jsonify({"results": results}), 200
+            return _corsify_actual_response(jsonify({"results": results})), 200
     except Exception as e:
-        return jsonify({"message": f"Error occurred while cloning repository or fetching metrics: {e}"}), 500
+        return _corsify_actual_response(jsonify({"message": f"Error occurred while cloning repository or fetching metrics: {e}"})), 500
+    
+@app.route("/", methods=["POST"])
+def health_check():
+    return _corsify_actual_response(jsonify({"status": "OK"})), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
