df471668ee6ebade2b596aaeef3413aff5577031
SiddhantShah14 <sshah267@asu.edu>
2025-04-27 19:25:49 -0700

    Resolved merge conflicts in crud.py; accepted incoming changes from recent commits.

--- middleware/database/crud.py
+++ middleware/database/crud.py
@@ -21,20 +21,28 @@ def save_metrics(service, project, commit, data, timestamp):
     except PyMongoError as e:
         print(f"[MongoDB] Failed to save metrics: {e}")
 
-def get_metrics(project_name: str, selected_metric: str):
+def get_metrics(project_name: str, selected_metrics: list[str]):
     try:
+
         db = MongoDBManager().get_db()
         all_data = {}
+        for metric in selected_metrics:
+            coll = get_collection(db, metric)
+            docs = list(coll.find({"projectName": project_name}))
+            for doc in docs:
+                if "_id" in doc:
+                    doc["_id"] = str(doc["_id"])
+        for metric in selected_metrics:
+            coll = get_collection(db, metric)
+            docs = list(coll.find({"projectName": project_name}))
+            for doc in docs:
+                if "_id" in doc:
+                    doc["_id"] = str(doc["_id"])
 
-        coll = get_collection(db, selected_metric)
-        docs = list(coll.find({"projectName": project_name}))
-        for doc in docs:
-            if "_id" in doc:
-                doc["_id"] = str(doc["_id"])
-
-        all_data[selected_metric] = docs  
-    
+            all_data[metric] = docs  
+        
         return all_data
+    
     except PyMongoError as e:
         print(f"[MongoDB] Failed to get metrics: {e}")
         return None
